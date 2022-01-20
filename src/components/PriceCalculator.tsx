6a10d81aad54143a926fac1853d5290660a61474
Peter Klingman <petercklingman@gmail.com>
2022-01-20 06:00:33 -0800

    list element and remove item function

--- src/components/PriceCalculator.tsx
+++ src/components/PriceCalculator.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, List } from "antd";
+import { Space, Button, List, Typography } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 import uniqid from "uniqid";
 
 import { PRICES } from "../constants/PRICES";
@@ -10,24 +11,51 @@ export const PriceCalculator: React.FC = () => {
   const [totalDiscount, setTotalDiscount] = useState(0);
   const [shoppingList, setShoppingList] = useState<ShoppingListType[]>([]);
 
+  const { Text } = Typography;
+  const { Item } = List;
+  const { Bread, Milk, Banana, Apple } = GroceriesEnum;
+
   const handleClickItem = (item: GroceriesEnum) => {
     setShoppingList(shoppingList.concat({ groceryType: item, id: uniqid() }));
+  };
+
+  const handleRemoveItem = (id: string) => {
+    setShoppingList(shoppingList.filter((item) => item.id !== id));
   };
 
   console.log("SHOPPING LIST", shoppingList);
 
   return (
     <>
-      <Button onClick={() => handleClickItem(GroceriesEnum.Bread)}>
-        Bread
-      </Button>
-      <Button onClick={() => handleClickItem(GroceriesEnum.Milk)}>Milk</Button>
-      <Button onClick={() => handleClickItem(GroceriesEnum.Banana)}>
-        Banana
-      </Button>
-      <Button onClick={() => handleClickItem(GroceriesEnum.Apple)}>
-        Apple
-      </Button>
+      <List
+        bordered
+        style={{ width: "50%", height: 600, overflow: "scroll" }}
+        dataSource={shoppingList}
+        size="small"
+        renderItem={({ groceryType, id }) => (
+          <Item
+            key={`list-${id}`}
+            extra={
+              <Button
+                type="ghost"
+                icon={<CloseOutlined />}
+                style={{ border: 0, color: "lightgray" }}
+                onClick={() => {
+                  handleRemoveItem(id);
+                }}
+              />
+            }
+          >
+            <Text>{groceryType}</Text>
+          </Item>
+        )}
+      />
+      <Space>
+        <Button onClick={() => handleClickItem(Bread)}>Bread</Button>
+        <Button onClick={() => handleClickItem(Milk)}>Milk</Button>
+        <Button onClick={() => handleClickItem(Banana)}>Banana</Button>
+        <Button onClick={() => handleClickItem(Apple)}>Apple</Button>
+      </Space>
     </>
   );
 };
