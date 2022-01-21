2ec06c4e488532820807b72d013494f79830d180
Peter Klingman <petercklingman@gmail.com>
2022-01-20 18:22:01 -0800

    readme updated and removed leftover cra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Button } from "antd";
 import "antd/dist/antd.min.css";
 import { PriceCalculator } from "./components/PriceCalculator";
-import "./App.css";
 
 function App() {
   return <PriceCalculator />;
